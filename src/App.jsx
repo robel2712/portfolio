cf5a6b04d3626947504042810d1c568135d22deb
Robel <robel.getaneh27@gmail.com>
2026-01-16 00:34:53 +0200

    vercel added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { ThemeProvider } from './context/ThemeContext';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -31,6 +33,8 @@ function App() {
       </main>
       <Footer />
     </div>
+    <SpeedInsights/>
+    <Analytics/>
     </ThemeProvider>
   );
 }
